983ee208d9601e802855ccfedef312eef26e34f5
mao520jin <15570670131@163.com>
2020-11-27 09:07:01 +0800

    去掉不可用的代码

--- jpkc/src/com/jpkc/service/SysAdminService.java
+++ jpkc/src/com/jpkc/service/SysAdminService.java
@@ -17,146 +17,52 @@ import com.jpkc.util.IDMaker;
 @Service
 public class SysAdminService {
 
-	private static Log log = LogFactory.getLog(SysAdminService.class);
+    private static Log log = LogFactory.getLog(SysAdminService.class);
 
-	@Resource
-	private SysAdminDao sysAdminDao;
+    @Resource
+    private SysAdminDao sysAdminDao;
 
-	public Page<SysAdmin> search(Map<String, Object> map) {
-		return sysAdminDao.search(map);
-	}
+    public Page<SysAdmin> search(Map<String, Object> map) {
+        return sysAdminDao.search(map);
+    }
 
-	public SysAdmin save(SysAdmin o) throws SQLException {
-		int row = 0;
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		log.info("row: " + row);
-		return row > 0 ? o : null;
-	}
-	public SysAdmin save(SysAdmin o) throws SQLException {
-		int row = 0;
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		log.info("row: " + row);
-		return row > 0 ? o : null;
-	}
-	public SysAdmin save(SysAdmin o) throws SQLException {
-		int row = 0;
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		log.info("row: " + row);
-		return row > 0 ? o : null;
-	}
-	public SysAdmin save(SysAdmin o) throws SQLException {
-		int row = 0;
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		log.info("row: " + row);
-		return row > 0 ? o : null;
-	}
-	public SysAdmin save(SysAdmin o) throws SQLException {
-		int row = 0;
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		if (o.getId() == null) {
-			o.setId(IDMaker.make());
-			row = sysAdminDao.insert(o);
-		} else {
-			row = sysAdminDao.update(o);
-		}
-		log.info("row: " + row);
-		return row > 0 ? o : null;
-	}
+    public SysAdmin save(SysAdmin o) throws SQLException {
+        int row = 0;
+        if (o.getId() == null) {
+            o.setId(IDMaker.make());
+            row = sysAdminDao.insert(o);
+        } else {
+            row = sysAdminDao.update(o);
+        }
+        if (o.getId() == null) {
+            o.setId(IDMaker.make());
+            row = sysAdminDao.insert(o);
+        } else {
+            row = sysAdminDao.update(o);
+        }
+        if (o.getId() == null) {
+            o.setId(IDMaker.make());
+            row = sysAdminDao.insert(o);
+        } else {
+            row = sysAdminDao.update(o);
+        }
+        log.info("row: " + row);
+        return row > 0 ? o : null;
+    }
 
-	public boolean delete(Long id) throws SQLException {
-		return sysAdminDao.delete(id) > 0;
-	}
+    public boolean delete(Long id) throws SQLException {
+        return sysAdminDao.delete(id) > 0;
+    }
 
-	/**
-	 * 
-	 * 登录
-	 * 
-	 * @param username
-	 * @param password
-	 * @return
-	 * @throws SQLException
-	 * 
-	 */
-	public SysAdmin login(String username, String password) throws SQLException {
-		return sysAdminDao.login(username, password);
-	}
+    /**
+     * 登录
+     *
+     * @param username
+     * @param password
+     * @return
+     * @throws SQLException
+     */
+    public SysAdmin login(String username, String password) throws SQLException {
+        return sysAdminDao.login(username, password);
+    }
 }
